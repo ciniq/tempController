0dbf229c3ba00b6c65626a16e73dd74b8701f673
Pim Kusters <p.kusters@mateum.nl>
2021-02-17 10:29:57 +0100

    dt added

--- tempcontroller.py
+++ tempcontroller.py
@@ -1,9 +1,11 @@
 import glob
 import time
+from datetime import datetime
 from AllSensors import AllSensors
 
 sensors = AllSensors(glob.glob('/sys/bus/w1/devices/28*'))
 
 while True:
     time.sleep(5)
-    print(sensors.getAllData())
+    dt = datetime.now()
+    print(sensors.getAllData() +' - ' + dt.strftime("%d/%m/%Y %H:%M:%S"))
