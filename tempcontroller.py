05930176b5c7946b0cba93e192477edcff9510cd
Pim Kusters <p.kusters@mateum.nl>
2021-02-22 22:01:21 +0100

    a

--- tempcontroller.py
+++ tempcontroller.py
@@ -8,5 +8,5 @@ sensors = AllSensors(glob.glob('/sys/bus/w1/devices/28*'))
 while True:
     time.sleep(60)
     dt = datetime.now()
-    sen = sensors.getAllData()
-    print(str(sen[0]) +' - ' + dt.strftime("%d/%m/%Y %H:%M:%S"))
+    for sen in sensors.getAllData() :
+        print(str(sen) +' - ' + dt.strftime("%d/%m/%Y %H:%M:%S"))
