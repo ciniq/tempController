b2e0b4bd7c03d38b45e82e0b0b2b12f07ed1f83e
Pim Kusters <p.kusters@mateum.nl>
2021-02-28 11:34:43 +0100

    a

---
+++ relayController/relaycontroler.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
+ 
+RELAIS_1_GPIO = 25
+RELAIS_2_GPIO = 18
+
+relais_1 = True
+relais_2 = False
+
+GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
+GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
+while True :
+    
+    if (relais_1 == True) :
+        relais_1 = False
+        GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
+    else :
+        relais_1 = True
+        GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
+    
+    if (relais_2 == True) :
+        relais_2 = False
+        GPIO.output(RELAIS_2_GPIO, GPIO.LOW) # out
+    else :
+        relais_2 = True
+        GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
+    
+
