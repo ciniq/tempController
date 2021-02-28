04bb3b00cdb3f82a85dc2c8f9c6edae7403b3e3b
Pim Kusters <p.kusters@mateum.nl>
2021-02-28 11:45:30 +0100

    a

--- relayController/relaycontroler.py
+++ relayController/relaycontroler.py
@@ -12,19 +12,21 @@ GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
 GPIO.setup(RELAIS_2_GPIO, GPIO.OUT) # GPIO Assign mode
 while True :
     sleep(1)
-    print('relais 1 '. ('aan' if relais_1 else 'uit'))
-    print('relais 2 '. ('aan' if relais_2 else 'uit'))
     if (relais_1 == True) :
+        print('relais 1 aan')
         relais_1 = False
         GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
     else :
+        print('relais 1 uit')
         relais_1 = True
         GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
     
     if (relais_2 == True) :
+        print('relais 2 aan')
         relais_2 = False
         GPIO.output(RELAIS_2_GPIO, GPIO.LOW) # out
     else :
+        print('relais 2 uit')
         relais_2 = True
         GPIO.output(RELAIS_2_GPIO, GPIO.HIGH) # on
     
